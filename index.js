528fef6fc4ced14b607ad2e90c6d0f9c5b56265b
Gabriel Ulloa <gsulloa@uc.cl>
2017-12-04 15:33:27 -0300

    Changed repo and added auth

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const prompt = require('co-prompt');
 const program = require('commander');
 const shell = require('shelljs');
 
-const REPO = "git@github.com:gsulloa/react-template.git"
+const REPO = "git@github.com:gsulloa/koa-boilerplate.git"
 
-function clone(repo, dir){
+function clone(dir, repo){
   shell.exec(`git clone ${repo} ${dir}`)
+}
+function pull(dir, type) {
+  shell.cd(dir)
+  shell.exec(`git pull origin ${type}`)
+  shell.cd("..")
 }
 function removeGit(dir) {
   shell.rm("-rf", `${dir}/.git`)
@@ -16,19 +21,23 @@ function removeGit(dir) {
 function createRepo(dir) {
   shell.cd(dir)
   shell.exec('git init && git add . && git commit -m "init commit"')
-  shell.exec('hub create// && git push origin master')
+  shell.exec('hub create && git push origin master')
   shell.cd("..")
 }
 
 program
   .arguments("<dir>")
   .option("-r, --repo <repo-url>", "URL repository to use as template")
+  .option("-a, --auth", "Start with auth")
   .action(function(dir) {
     co(function *(){
       const repo = program.repo ? program.repo : REPO;
-      clone(repo, dir);
+      clone(dir, repo);
+      if (program.auth) {
+        pull("auth")
+      }
       removeGit(dir);
-      createRepo(dir);
+      // createRepo(dir);
     })
   })
   .parse(process.argv)
